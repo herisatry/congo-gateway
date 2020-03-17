2d7c253b600c24841b28cf94265826cefd2241ac
herisatry <herisatry@gmail.com>
2020-03-17 13:22:22 +0200

    navbar upper section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,25 +34,42 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
+      <div >
+        <div className="has-background-black has-text-white">
+        <nav className=" container level ">
+          <div className={` level-left`}>
+            <p className="level-item">
+              Heures d'ouverture:<br/> Lundi - Vendredi : 06:00 - 16:00, Samedi - Fermé
+            </p>
+          </div>
+          <div className={`level-right`}>
+            <p className="level-item ">
+              <Link to="/contact" className="navbar-item">Nous contacter ?</Link>
+            </p>
+          </div>
+      </nav>
+        </div>
       <nav className="level ">
-  <div className={`level-item has-text-centered ${this.state.navBarActiveClass}`}>
-  <p className="level-item has-text-centered">
-    <Link to="/" className="navbar-item">Acceuil</Link>
-  </p>
-  <p className="level-item has-text-centered">
-  <Link to="/about" className="navbar-item">A propos</Link>
-  </p>
-  <p className="level-item has-text-centered">
-    <img src={logo} alt="logo" style={{ width: '14em', height: '10em' }}/>
-  </p>
-  <p className="level-item has-text-centered">
-  <Link to="/blog" className="navbar-item">Blog</Link>
-  </p>
-  <p className="level-item has-text-centered">
-  <Link to="/contact" className="navbar-item">Contact</Link>
-  </p>
-  </div>
-</nav>
+          <div className={`level-item has-text-centered ${this.state.navBarActiveClass}`}>
+            <p className="level-item has-text-centered">
+              <Link to="/" className="navbar-item">Acceuil</Link>
+            </p>
+            <p className="level-item has-text-centered">
+              <Link to="/about" className="navbar-item">A propos</Link>
+            </p>
+            <p className="level-item has-text-centered">
+              <img src={logo} alt="logo" style={{ width: '14em', height: '10em' }}/>
+            </p>
+            <p className="level-item has-text-centered">
+              <Link to="/blog" className="navbar-item">Blog</Link>
+            </p>
+            <p className="level-item has-text-centered">
+              <Link to="/contact" className="navbar-item">Contact</Link>
+            </p>
+          </div>
+      </nav>
+    
+    </div>
     )
   }
 }
